bd8f6544a8a39172429a6741d3e778502606363c
Henry Braid <henrybraid@gmail.com>
2024-03-25 12:30:58 -0700

    Initial Commit

---
+++ umbGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  umbGame
+//
+//  Created by Henry Braid on 3/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
