91a621d6ca7c918976977ea1e14f1e3befce7fed
Henry Braid <henrybraid@gmail.com>
2024-03-28 20:24:01 -0700

    adjusted sizes of the images and added coloring to the dog

--- umbGame/main.cpp
+++ umbGame/main.cpp
@@ -1,11 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
-
+#include <cstdlib>
+#include <time.h>
+#include <stdlib.h>
+#include <vector>
 
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 800
-#define RAIN_LENGTH 20
+#define RAIN_LENGTH 30
 
 bool bordercolli(sf::Sprite entity) {
     if(entity.getPosition().x + entity.getGlobalBounds().width > 1024 || entity.getPosition().x < 0){
@@ -14,6 +17,13 @@ bool bordercolli(sf::Sprite entity) {
     return 0;
 }
 
+bool turn() {
+    int num = rand() % 2;
+    if (num == 0)
+        return true;
+    return false;
+
+}
 
 bool checkBorderRight(sf::Sprite entity)
 {
@@ -42,6 +52,19 @@ bool checkBorderBottom(sf::Sprite entity)
     return true;
 }
 
+
+bool checkEntityCollision(sf::Sprite entity1, sf::Sprite entity2)
+{
+    if(entity1.getGlobalBounds().intersects(entity2.getGlobalBounds()))
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+
+
 int randRainGen() //pick a random raindrop in the raindrop vector
 {
     int range = RAIN_LENGTH;
@@ -55,9 +78,9 @@ std::vector<sf::Sprite> rainSpawner(std::vector<sf::Sprite> raindrops, sf::Sprit
     {
         raindrops.push_back(rain);
     }
-    for(int i = 1; i<RAIN_LENGTH;i++)
+    for(int i = 0; i<RAIN_LENGTH;i++)
     {
-        raindrops[i].setPosition(rand()%SCREEN_WIDTH,-10);
+        raindrops[i].setPosition(rand()%(SCREEN_WIDTH-10),-10);
     }
     
     return raindrops;
@@ -67,64 +90,71 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH,SCREEN_HEIGHT), "Umbrella Game");
     
-    //dog texture setup
+    //textures
+    sf::Texture background;
     sf::Texture dog_texture;
-    //dog_texture.loadFromFile("/Users/henrybraid/XCode Projects/Umbrella Game/tempdog.png");
-    if(!dog_texture.loadFromFile("/Users/henrybraid/XCode Projects/umbGame/umbGame/images/tempdog.png"))
+    sf::Texture bird_texture;
+    sf::Texture drop_texture;
+    
+    
+    drop_texture.setSmooth(true);
+    drop_texture.setRepeated(true);
+    
+    
+    if(!dog_texture.loadFromFile("/Users/henrybraid/XCode Projects/umbGame/umbGame/images/tempdog.png") ||!bird_texture.loadFromFile("/Users/henrybraid/XCode Projects/umbGame/umbGame/images/tempbird.gif.png")||!drop_texture.loadFromFile("/Users/henrybraid/XCode Projects/umbGame/umbGame/images/WaterDrop.png")||!background.loadFromFile("/Users/henrybraid/XCode Projects/umbGame/umbGame/images/back.png"))
     {
         return EXIT_FAILURE;
     }
+    
+    sf::Sprite back;
+    back.setTexture(background);
+    back.scale(5, 5);
+    back.setPosition(0, 0);
+    
     sf::Sprite dog;
-    dog.setPosition(200,600);
+    dog.setPosition(200,685);
     dog.setTexture(dog_texture);
     dog.setScale(0.5,0.5);
+    float xv = 0.05f;
     
-    //bird texture setup
-    sf::Texture bird_texture;
-    if(!bird_texture.loadFromFile("/Users/henrybraid/XCode Projects/umbGame/umbGame/images/tempbird.gif.png"))
-    {
-        return EXIT_FAILURE;
-    }
     sf::Sprite bird;
     bird.setTexture(bird_texture);
-    bird.setScale(0.60,0.60);
+    bird.setScale(0.7,0.7);
     bird.setPosition(400, 400);
     
-    sf::Texture droplet;
-    droplet.loadFromFile("/Users/henrybraid/XCode Projects/umbGame/umbGame/images/WaterDrop.png");
+    
     sf::Sprite rain;
-    rain.setTexture(droplet);
-    rain.setScale(0.175,0.175);
+    rain.setTexture(drop_texture);
+    rain.setScale(0.125,0.125);
     
     
     std::vector<sf::Sprite> raindrops;
     raindrops = rainSpawner(raindrops, rain);
    
     
-    
-    
-    float xv = 0.05f;
+    sf::Clock clock;
+    float timer = 0.0f;
+    float timerMax = 1.0f;
+    float yv = 0.15f;
+    float b_xv = 0;
+    int hurt_timer=0; //used to measure how long to flash the dog red... indicating collision with raindrop
     float b_changex=0;
-    float yv=0.15f; //speed of rain falling
     
-    
-    int clock = 0;
     while(window.isOpen())
     {
-        int num = randRainGen();
-        clock++;
+        
+        
         sf::Event event; //read for an event from the user
         
         while(window.pollEvent(event))
         {
-            
             switch ( event.type )
             {
                 case sf::Event::Closed:
                     window.close();
                     break;
                 case sf::Event::KeyPressed: //read key input
-                    
+                    b_changex=0; //reset direction
                     //read input of A or Left
                     if((event.key.code==sf::Keyboard::A ||event.key.code==sf::Keyboard::Left) &&checkBorderLeft(bird))
                     {
@@ -144,27 +174,44 @@ int main()
             }
              
         }
-        for(int j = 0; j<num; j++)
-        {
-            if(checkBorderBottom(raindrops[j])&&(raindrops[j].getPosition().y!=bird.getPosition().y))
-            {
-                raindrops[j].move(0,yv);
-            }
-            else{
-                int xpos=raindrops[j].getPosition().x;
-                raindrops[j].setPosition(xpos,-10);
-            }
-        }
+        
         
             
         if (bordercolli(dog)) {
                     xv = xv * -1.0f;
                 }
-        dog.move(xv, 0);
+        timer += clock.restart().asSeconds();
 
+        if (timer >= timerMax) {
+            if (turn()) {
+                xv = -xv;
+            }
+            timer = 0.0f;
+        }
+        dog.move(xv, 0);
+        
+        int num = randRainGen();
+        for(int j = 0; j<num; j++)
+        {
+            if(checkEntityCollision(raindrops[j],dog))
+            {
+                dog.setColor(sf::Color(255,0,0));
+                hurt_timer=0;
+            }
+            else if(checkBorderBottom(raindrops[j]) && !checkEntityCollision(raindrops[j], bird))
+            {
+                raindrops[j].move(0,yv);
+                continue;
+            }
+            
+            int xpos=rand()%(SCREEN_WIDTH-10);
+            raindrops[j].setPosition(xpos,-10);
+            
+        }
         
         window.clear();
         //draw objects
+        window.draw(back);
         window.draw(bird);
         window.draw(dog);
         for(int j = 0; j<RAIN_LENGTH; j++)
@@ -172,6 +219,11 @@ int main()
             window.draw(raindrops[j]);
         }
         window.display();
+        if(hurt_timer>500)
+        {
+            dog.setColor(sf::Color::White);
+        }
+        hurt_timer++;
     }
     return EXIT_SUCCESS;
 }
